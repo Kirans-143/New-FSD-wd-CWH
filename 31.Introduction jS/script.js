843b73367cda964cae8276539b2bf77c9a5e0c70
Kirans-143 <kiran.shet77@gmail.com>
2024-06-11 19:41:42 +0530

    Introduction To JAVASCRIPT

---
+++ 31.Introduction jS/script.js
@@ -0,0 +1,14 @@
+alert("Hello world");
+
+console.log("Code is running");
+
+var a = prompt("Enter your number");
+
+var isTrue = confirm("are you sure you want to leave ");
+
+if (isTrue) {
+  console.log("you are leaving ");
+} else {
+  console.log("you are not leaving");
+}
+console.log("Your number is " + a);
